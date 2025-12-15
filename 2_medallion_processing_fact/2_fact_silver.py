be5adcd59182e82335559169d36f9c8662c12f76
Swapnil Vasant Taware <98303181+itsSwapnil@users.noreply.github.com>
2025-12-15 11:54:51 +0530

    Implement data cleansing and transformation for orders
    This script performs data cleansing and transformation on order items, including type conversions and cleaning of various fields.

---
+++ 2_medallion_processing_fact/2_fact_silver.py
@@ -0,0 +1,98 @@
+# Databricks notebook source
+## Bronze to Silver: Data Cleansing and Transformation
+
+from pyspark.sql.types import StringType, IntegerType, DateType, BooleanType
+import pyspark.sql.functions as F
+
+
+catalog_name = 'ecommerce'
+
+
+df = spark.table(f'{catalog_name}.bronze.brz_order_items')
+df.show(5)
+
+
+df.printSchema()
+
+
+df = df.dropDuplicates(["order_id", "item_seq"])
+
+# Transformation: Convert 'Two' → 2 and cast to Integer
+df = df.withColumn(
+    "quantity",
+    F.when(F.col("quantity") == "Two", 2).otherwise(F.col("quantity")).cast("int")
+)
+
+# Transformation : Remove any '$' or other symbols from unit_price, keep only numeric
+df = df.withColumn(
+    "unit_price",
+    F.regexp_replace("unit_price", "[$]", "").cast("double")
+)
+
+# Transformation : Remove '%' from discount_pct and cast to double
+df = df.withColumn(
+    "discount_pct",
+    F.regexp_replace("discount_pct", "%", "").cast("double")
+)
+
+
+df = df.withColumn(
+    "coupon_code", F.lower(F.trim(F.col("coupon_code")))
+)
+
+# Transformation : channel processing 
+df = df.withColumn(
+    "channel",
+    F.when(F.col("channel") == "web", "Website")
+    .when(F.col("channel") == "app", "Mobile")
+    .otherwise(F.col("channel")),
+)
+
+
+
+# Transformation: datatype conversions
+# 1) Convert dt (string → date)
+df = df.withColumn(
+    "dt",
+    F.to_date("dt", "yyyy-MM-dd")     
+)
+
+# 2) Convert order_ts (string → timestamp)
+df = df.withColumn(
+    "order_ts",
+    F.coalesce(
+        F.to_timestamp("order_ts", "yyyy-MM-dd HH:mm:ss"),  # matches 2025-08-01 22:53:52
+        F.to_timestamp("order_ts", "dd-MM-yyyy HH:mm")      # fallback for 01-08-2025 22:53
+    )
+)
+
+
+# 3) Convert item_seq (string → integer)
+df = df.withColumn(
+    "item_seq",
+    F.col("item_seq").cast("int")
+)
+
+# 4) Convert tax_amount (string → double, strip non-numeric characters)
+df = df.withColumn(
+    "tax_amount",
+    F.regexp_replace("tax_amount", r"[^0-9.\-]", "").cast("double")
+)
+
+
+#Transformation : Add processed time 
+df = df.withColumn(
+    "processed_time", F.current_timestamp()
+)
+
+
+display(df.limit(5))
+
+df.printSchema()
+
+
+# Write raw data to the silver layer (catalog: ecommerce, schema: silver, table: slv_brands)
+df.write.format("delta") \
+    .mode("overwrite") \
+    .option("mergeSchema", "true") \
+    .saveAsTable(f"{catalog_name}.silver.slv_order_items")
